69fd5855e7a7b63a0ce323e62ac7c34cd269eb00
ben5962 <leclere.corentin@gmail.com>
2020-06-07 12:56:46 +0200

    interroger xrandr sur noms ecrans dispo

--- resolution.sh
+++ resolution.sh
@@ -2,7 +2,8 @@
 #http://superuser.com/questions/311378/how-to-get-a-higher-resolution-on-ubuntu-11-04-using-an-intel-chipset
 
 RESOLUTION=$(cvt 1440 900 | sed -ne '2p' |cut -d' ' -f 3-14)
+ECRAN=$(xrandr |grep -E "\bconnected\b" |cut -d' ' -f 1
 xrandr --newmode NORMAL "${RESOLUTION}"
-xrandr --addmode VGA1 NORMAL
-xrandr --output VGA1 --mode NORMAL
+xrandr --addmode "${ECRAN}" NORMAL
+xrandr --output "${ECRAN}" --mode NORMAL
 
